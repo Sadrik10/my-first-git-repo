13da0a16c8dfce5b8b3a494e72f6c4a9aee1e655
Sadrettin Akkoyun <sadrettin.akkoyun10@gmail.com>
2022-04-04 22:28:23 -0400

    day02_second_third_classes

---
+++ src/first_git_package/SecondJavaClass.java
@@ -0,0 +1,6 @@
+package first_git_package;
+
+public class SecondJavaClass {
+
+    // This will have some code
+}
---
+++ src/first_git_package/ThirdJavaClass.java
@@ -0,0 +1,5 @@
+package first_git_package;
+
+public class ThirdJavaClass {
+    // This will also have some java code
+}
